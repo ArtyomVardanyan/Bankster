2adcde91756a309958fd1eec97753790d2126817
Emin291 <emin.koganyan.y@tumo.org>
2022-07-22 15:25:31 +0400

    Login Page

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import LoginPage from './src/components/pages/LoginPage';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <LoginPage />
     </View>
   );
 }
@@ -13,7 +12,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#120E0B',
     alignItems: 'center',
     justifyContent: 'center',
   },
